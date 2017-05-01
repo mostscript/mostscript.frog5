d4c25f5d7a767b42002062463360dc7111788818
Sean Upton <sdupton@gmail.com>
2017-05-01 11:26:41 -0600

    Permissions and roles configured for collective.teamwork (data entry permission and associated role, config for workgroups plugged in).

--- upiq/qits/zope2.py
+++ upiq/qits/zope2.py
@@ -1,3 +1,24 @@
+import config
+
 
 def initialize(context):
-    pass  # make a zope 2 product
+    config.register()  # register plugin roles for collective.teamwork
+    patch_access_logging()
+
+
+def patch_access_logging():
+    """
+    Monkey patch access logging with a wrapper that returns
+    before emitting any messages for OPTIONS requests from haproxy
+    health checks.
+    """
+    from ZServer.AccessLogger import AccessLogger
+    _orig = AccessLogger.log
+
+    def newlog(self, message):
+        if 'OPTIONS / HTTP/1.0' in message:
+            return  # do not emit log message for haproxy health check
+        _orig(self, message)
+
+    setattr(AccessLogger, 'log', newlog)
+
